c60a72d34591bfe4039d497b8f88df3c7819c785
Zuo <czuo3@illinois.edu>
2018-10-22 16:16:18 -0500

    keyboard comment czuo3

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -2,7 +2,8 @@
 #include "i8259.h"
 #include "scan_code.h"
 
-
+/* initialization of keyboard*/
 extern void init_keyboard(void);
 
+/* keyboard handler body function */
 extern void keyboard_handler(void);
