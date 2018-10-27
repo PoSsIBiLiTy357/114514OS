a982c3b7f6212dd318bf5530a2556b50cbc2009d
Zuo <czuo3@illinois.edu>
2018-10-26 20:40:27 -0500

    pinying comment best comment

--- student-distrib/scan_code.c
+++ student-distrib/scan_code.c
@@ -39,9 +39,23 @@
 #define   N		49
 #define   M		50
 
+#define CAPSLOCK	58
+
+
+#define 	DASH	12
+#define 	EQUAL	13
+#define 	LSB		26
+#define 	RSB		27
+#define		FENHAO	0x27
+#define		SQ		0x28
+#define 	COMMA	51
+#define 	POINT	52
+
+#define NUM_CONVERT 80
 
 /* array to store all scan codes */
 char scan_code[NUM_SCAN_CODES];
+char shift_convert[NUM_CONVERT];
 
 
 /*
@@ -60,6 +74,45 @@ void init_scan_code(){
 	for (i = 0; i < NUM_SCAN_CODES; i++){
 		scan_code[i]=0;
 	}
+	for (i = 0; i < NUM_CONVERT; i++){
+		shift_convert[i]=0;
+	}
+	
+	shift_convert[Q]='Q';
+	shift_convert[W]='W';
+	shift_convert[E]='E';
+	shift_convert[R]='R';
+	shift_convert[T]='T';
+	shift_convert[Y]='Y';
+	shift_convert[U]='U';
+	shift_convert[I]='I';
+	shift_convert[O]='O';
+	shift_convert[P]='P';
+	shift_convert[A]='A';
+	shift_convert[S]='S';
+	shift_convert[D]='D';
+	shift_convert[F]='F';
+	shift_convert[G]='G';
+	shift_convert[H]='H';
+	shift_convert[J]='J';
+	shift_convert[K]='K';
+	shift_convert[L]='L';
+	shift_convert[Z]='Z';
+	shift_convert[X]='X';
+    shift_convert[C]='C';
+	
+	shift_convert[ONE]='!';
+	shift_convert[TWO]='@';
+	shift_convert[THREE]='#';
+	shift_convert[FOUR]='$';
+	shift_convert[FIVE]='%';
+	shift_convert[SIX]='^';
+	shift_convert[SEVEN]='&';
+	shift_convert[EIGHT]='*';
+	shift_convert[NINE]='(';
+	shift_convert[TEN]=')';
+	
+	
 	
 	scan_code[Q]='q';
 	scan_code[W]='w';
@@ -98,5 +151,8 @@ void init_scan_code(){
 	scan_code[EIGHT]='8';
 	scan_code[NINE]='9';
 	scan_code[TEN]='0';
+	
+	scan_code[]=;
+	
 	
 }
