e1b93f96ea2ffc5eec90babb433a54ae4849867f
Chen <andrew6@illinois.edu>
2018-11-12 13:49:47 -0600

    comments

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -9,58 +9,98 @@
 #define START_ADDR        24
 #define ADDR_OFFSET        8
 
-#define PROGRAM_IMAGE_ADDR 0x8048000
-#define PROC_NUM 6
-#define PCB_SIZE 0x2000
-#define KSTACK_BOT 0x7fe000
-#define ASM 1
+#define KSTACK_START        0x800000
+#define PROGRAM_IMAGE_ADDR  0x8048000
+#define PROC_NUM            6
+#define PCB_SIZE            0x2000
+#define KSTACK_BOT          0x7fe000
+#define ASM                 1
+#define RESERV_FILES        2
 
+#define FILE_RTC            0
+#define FILE_DIR            1
+#define FILE_REG            2
+
+
+/* Current PID */
 static int curr = 0;
-//device_t rtc = { rtc_read, rtc_write, rtc_open, rtc_close };
+
+/* Table of active and inactive processes (active = 1, inactive = 0) */
 static int proc_state[PROC_NUM] = {0, 0, 0, 0, 0, 0};
 
 
-void pcb_init(int pid){
+/*
+* pcb_init
+*   DESCRIPTION: Initializes a new process control block for a process given
+*       a process identification number (PID).
+*
+*   INPUTS: int pid - process identification number for process to be initialized
+*   OUTPUTS: none
+*   RETURN VALUE: none
+*	SIDE EFFECTS : Creates a new PCB for the process with requested PID
+*/
+void pcb_init(int pid) {
     int i;
     curr = pid;
     
+    /* Create PCB for current process and assign PID */
     pcb_t* pcb= (pcb_t *)(KSTACK_BOT - PCB_SIZE * pid);
     pcb->pid=pid;
-    pcb->file_array[0].read = terminal_read_wrap;///init stdin and stdout     /* compiler error */
-    pcb->file_array[0].write=NULL;
-    pcb->file_array[0].open=NULL;
-    pcb->file_array[0].close=NULL;
-    pcb->file_array[0].flag=1;
 
-    pcb->file_array[1].read=NULL;
-    pcb->file_array[1].write= terminal_write_wrap;                           /* compiler error */
-    pcb->file_array[1].open=NULL;
-    pcb->file_array[1].close=NULL;
-    pcb->file_array[1].flag=1;
+    /* Fill in file descriptors for reserved file stdin for every new process */
+    pcb->file_array[0].read  = terminal_read_wrap;  //init stdin and stdout
+    pcb->file_array[0].write = NULL;
+    pcb->file_array[0].open  = NULL;
+    pcb->file_array[0].close = NULL;
+    pcb->file_array[0].flag  = 1;
 
-    for(i=2;i<FDESC_SIZE;i++){
-        pcb->file_array[i].flag=0;
+    /* Fill in file descriptors for reserved file stdout for every new process */
+    pcb->file_array[1].read  = NULL;
+    pcb->file_array[1].write = terminal_write_wrap;
+    pcb->file_array[1].open  = NULL;
+    pcb->file_array[1].close = NULL;
+    pcb->file_array[1].flag  = 1;
+
+
+    /* Set remaining files as unused (flag = 0) for every new process */
+    for (i = RESERV_FILES; i < FDESC_SIZE; i++) {
+        pcb->file_array[i].flag = 0;
     }
-    if(pid==0){
-        pcb->parent=NULL;
+
+
+    /* Check if this is the first process, and if it is, set parent ptr to NULL */
+    if (pid == 0) {
+        pcb->parent = NULL;
     }
-    else{
+    else {
         pcb->parent = (int32_t *)(KSTACK_BOT - PCB_SIZE * pid + PCB_SIZE);
     }
 }
 
+/*
+* get_pid
+*   DESCRIPTION: Finds and returns a valid PID based on how many processes
+*       are currently active. Returns -1 if max number of processes (six)
+*       are already active.
+*
+*   INPUTS: none
+*   OUTPUTS: int - PID (0-5)
+*   RETURN VALUE: none
+*/
 int get_pid(){
     int i;
-    for(i=0;i<PROC_NUM;i++){
-        if(proc_state[i]==0){
+
+    /* Look thru process table for an inactive process (marked 0) and return it */
+    for(i = 0; i < PROC_NUM; i++){
+        if(proc_state[i] == 0){
             return i;
         }
     }
+
+    /* Return -1 max number of processes allowed are already in use */
     return -1;
 }
 
-
-//device_t rtc = { rtc_read, rtc_write, rtc_open, rtc_close };
 
 int32_t halt(uint8_t status){
 
@@ -121,24 +161,19 @@ int32_t halt(uint8_t status){
 *   RETURN VALUE: 0 on success, -1 on failure
 *	SIDE EFFECTS : Switches processor to user mode to run given user program
 */
-
 int32_t execute(const uint8_t * command){
     uint8_t inFile[CMD_LIMIT];  /* name of executable file           */
     uint32_t v_addr;            /* virtual addr of first instruction */
     dentry_t d;
     int pid;
-    //u parse/check
-    /* Ensure the given command is a valid executable file */
-
     
+    /* Ensure the given command is a valid executable file */   
     if (verify_file(command, inFile, &v_addr) == -1) { return -1; }
     //134513384 080482E8
     //fetch a proccess id that is not in use 
     pid=get_pid();
-    proc_state[pid]=1;
+    proc_state[pid] = 1;
 
-
-   
     //u Create PCB
     pcb_init(pid);
 
@@ -165,31 +200,33 @@ int32_t execute(const uint8_t * command){
     read_dentry_by_name(inFile, &d);
     read_f(d.inode, (uint8_t *)PROGRAM_IMAGE_ADDR);
 
+    /* Upate TSS ss0 and esp0 */
     tss.ss0 = KERNEL_DS;
     tss.esp0 = (0x100000*8) - PCB_SIZE * pid - 4;
 
+    /* IRET setup and context switch */
     asm volatile(
-                //  "cli;"
-                 "mov $0x2B, %%ax;"
-                 "mov %%ax, %%ds;"
-                 "movl $0x83FFFFC, %%eax;"
-                 //"movl $0x8400000, %%eax;"
-                 "pushl $0x2B;"
-                 "pushl %%eax;"
-                 "pushfl;"
-                //  "popl %%edx;"
-                //  "orl $0x200, %%edx;"
-                //  "pushl %%edx;"
-                 "pushl $0x23;"
-                 "pushl %0;"
-                 "iret;"
-                 "RETURN_FROM_IRET:;"
-                 "LEAVE;"
-                 "RET;"
-                 :	/* no outputs */
-                 :"r"(v_addr)	/* input */
-                 :"%edx","%eax"	/* clobbered register */
-                 );
+        //  "cli;"
+            "mov $0x2B, %%ax;"
+            "mov %%ax, %%ds;"
+            "movl $0x83FFFFC, %%eax;"
+            //"movl $0x8400000, %%eax;"
+            "pushl $0x2B;"
+            "pushl %%eax;"
+            "pushfl;"
+        //  "popl %%edx;"
+        //  "orl $0x200, %%edx;"
+        //  "pushl %%edx;"
+            "pushl $0x23;"
+            "pushl %0;"
+            "iret;"
+            "RETURN_FROM_IRET:;"
+            "LEAVE;"
+            "RET;"
+            :	            /* no outputs         */
+            :"r"(v_addr)	/* input              */
+            :"%edx","%eax"	/* clobbered register */
+            );
 
     return 0;
 }
@@ -210,7 +247,7 @@ int32_t execute(const uint8_t * command){
                    user address into v_addr
 */
 int8_t verify_file(const uint8_t * cmd, uint8_t inFile[CMD_LIMIT], uint32_t * v_addr) {
-    int i;
+    int i;  /* Loop index */
 
     /* Make sure passed in ptr is not a nullptr */
     if (cmd == NULL) { return -1; }
@@ -233,82 +270,164 @@ int8_t verify_file(const uint8_t * cmd, uint8_t inFile[CMD_LIMIT], uint32_t * v_
     if (strncmp(fileBuf, magicBuf, BYTE_LEN)) { return -1; }
 
     /* Retrieve address for first instruction from bytes 24-27 */
-    read_f_by_name(inFile, 24, (uint8_t*)v_addr, BYTE_LEN);
+    read_f_by_name(inFile, START_ADDR, (uint8_t*)v_addr, BYTE_LEN);
 
     return 0;
 }
 
+
+/*
+* read
+*   DESCRIPTION: Reads a given file by creating a temporary PCB to retrieve the
+*       starting offset and inode for that specific file and then jump to the
+*       respective read handler for the file
+*
+*   INPUTS: uint32_t fd - file descriptor
+*           void * buf - buffer to fill up with chars from file
+*           int32_t nbytes - how many characters to read 
+*   OUTPUTS: number of characters read
+*   RETURN VALUE: int32_t - number of characters read 
+*                 -1 on failure (no characters to read)
+*/
 int32_t read(int32_t fd, void * buf, int32_t nbytes){
-    if (fd>= FDESC_SIZE) return -1;
+    /* Check that we haven't exceeded max number of open files */
+    if (fd >= FDESC_SIZE) return -1;
+
+    /* Create a temporary PCB and initalize offest and inode for file to then jump to read() */
     pcb_t * temp_pcb = (pcb_t*) KSTACK_BOT-curr*PCB_SIZE;
     int offset = temp_pcb->file_array[fd].file_pos;
     int inode = temp_pcb->file_array[fd].inode;
     int count = temp_pcb->file_array[fd].read(inode,offset,buf,nbytes);
-    if (count<0) return -1;
+
+    /* Check if read handler returned an error, if so, return -1 */
+    if (count < 0) return -1;
+
+    /* Increment the read offset */
     temp_pcb->file_array[fd].file_pos += count;
+
     return count;
     
 }
+
+
+/*
+* write
+*   DESCRIPTION: Writes to a given file by creating a temporary PC to retrive
+*       file inode and jump to the respective write handler for file 
+*
+*   INPUTS: uint32_t fd - file descriptor
+*           void * buf - buffer to write to file
+*           int32_t nbytes - how many characters were written
+*   OUTPUTS: number of characters written
+*   RETURN VALUE: int32_t - number of characters written
+*                 -1 on failure (no characters to written)
+*/
 int32_t write(int32_t fd, const void * buf, int32_t nbytes){
-    
+    /* Check that we haven't exceeded max number of open files */
     if (fd >= FDESC_SIZE) return -1;
+
+    /* Create a temporary PCB and initalize offest and inode for file to then jump to write() */
     pcb_t * temp_pcb = (pcb_t*) KSTACK_BOT-curr*PCB_SIZE;
     int inode = temp_pcb->file_array[fd].inode;
     int count = temp_pcb->file_array[fd].write(inode, 0, (uint8_t *)buf, nbytes);
-    if (count <0) return -1;
+
+    /* Check if read handler returned an error, if so, return -1 */
+    if (count < 0) return -1;
+
     return count;
 }
+
+
+/*
+* open
+*   DESCRIPTION: Opens a file and checks for what file type it is to
+*       give it its proper initializations
+*
+*   INPUTS: const uint8_t * filename
+*   OUTPUTS: file index
+*/
 int32_t open(const uint8_t * filename){
-    
-    pcb_t pcb_t;
+    pcb_t pcb;
     dentry_t temp_dentry;
     int i;
-    if (read_dentry_by_name(filename, &temp_dentry) ==-1) return -1; // if the filename does not exist, return -1
-    for (i = 0; i <FDESC_SIZE;i++){
-        if (pcb_t.bitmap[FDESC_SIZE]==0){
-            pcb_t.bitmap[FDESC_SIZE]=1;
+
+    /* If the filename does not exist, return -1 */
+    if (read_dentry_by_name(filename, &temp_dentry) ==-1) return -1; 
+    
+    for (i = 0; i < FDESC_SIZE; i++) {
+        if (pcb.bitmap[FDESC_SIZE] == 0) {
+            pcb.bitmap[FDESC_SIZE] = 1;
             break;
         } 
     }
-    if (i==FDESC_SIZE) return -1; //if no descriptors are free, return -1
+    if (i == FDESC_SIZE) return -1; //if no descriptors are free, return -1
     
-    if(temp_dentry.ftype == 0){
-        pcb_t.file_array[i].read = rtc_read_wrapper;
-        pcb_t.file_array[i].write = rtc_write_wrapper;
-        pcb_t.file_array[i].open = rtc_open_wrapper;
-        pcb_t.file_array[i].close = rtc_close_wrapper;
+    /* Check if the file type is an RTC file */
+    if (temp_dentry.ftype == FILE_RTC) {
+        pcb.file_array[i].read  = rtc_read_wrapper;
+        pcb.file_array[i].write = rtc_write_wrapper;
+        pcb.file_array[i].open  = rtc_open_wrapper;
+        pcb.file_array[i].close = rtc_close_wrapper;
     }
     
-    if (temp_dentry.ftype == 1){
-        pcb_t.file_array[i].read = read_dir_wrapper;
-        pcb_t.file_array[i].write = write_dir_wrapper;
-        pcb_t.file_array[i].open = open_dir_wrapper;
-        pcb_t.file_array[i].close = close_dir_wrapper;        
+    /* Check if file type is a directory file */
+    if (temp_dentry.ftype == FILE_DIR) {
+        pcb.file_array[i].read  = read_dir_wrapper;
+        pcb.file_array[i].write = write_dir_wrapper;
+        pcb.file_array[i].open  = open_dir_wrapper;
+        pcb.file_array[i].close = close_dir_wrapper;        
     }
-    if (temp_dentry.ftype ==2){
-        pcb_t.file_array[i].read = read_f_wrapper;
-        pcb_t.file_array[i].write = write_f_wrapper;
-        pcb_t.file_array[i].open = open_f_wrapper;
-        pcb_t.file_array[i].close = close_f_wrapper;           
+
+    /* Check if file type is a regular file */
+    if (temp_dentry.ftype == FILE_REG) {
+        pcb.file_array[i].read  = read_f_wrapper;
+        pcb.file_array[i].write = write_f_wrapper;
+        pcb.file_array[i].open  = open_f_wrapper;
+        pcb.file_array[i].close = close_f_wrapper;           
     }
-    pcb_t.file_array[i].inode = temp_dentry.inode;
-    pcb_t.file_array[i].flag =1; // file in use 
-    pcb_t.file_array[i].file_pos = 0;
+
+    /* Set the inode for current file and set starting offset to 0 */
+    pcb.file_array[i].inode = temp_dentry.inode;
+    pcb.file_array[i].flag = 1; // file in use 
+    pcb.file_array[i].file_pos = 0;
+
     return 0;
 
 }
 
+
+/*
+* close
+*   DESCRIPTION: Closes a given file and updates the file array 
+*
+*   INPUTS: int32_t fd
+*   OUTPUTS: 0 on success, -1 on fail
+*/
 int32_t close(int32_t fd){
-    if (fd>=FDESC_SIZE) return -1;
-    pcb_t * temp_pcb;
-    int temp_pcb_addr = 0x800000-0x2000-curr*0x2000;
-    temp_pcb = (pcb_t *) temp_pcb_addr;
-    temp_pcb->file_array[fd].flag =0;
-    return 0;
+    /* Make sure given file descriptor is within range */
+    if (fd >= FDESC_SIZE) return -1;
 
+    pcb_t * temp_pcb;
+    int temp_pcb_addr;
+
+    temp_pcb_addr = KSTACK_START - PCB_SIZE - (curr * PCB_SIZE);
+    //int temp_pcb_addr = 0x800000-0x2000-curr*0x2000;
+    temp_pcb = (pcb_t *) temp_pcb_addr;
+    temp_pcb->file_array[fd].flag = 0;
+   
+    return 0;
 }
 
 
+/*
+*  getargs
+*   DESCRIPTION: 
+*
+*   INPUTS: uint8_t * buf
+*           int32_t nbytes
+*   OUTPUTS: 
+*   RETURN VALUE: 
+*/
 int32_t getargs(uint8_t * buf, int32_t nbytes){
     // if(){
     //     return -1;
@@ -318,12 +437,42 @@ int32_t getargs(uint8_t * buf, int32_t nbytes){
     // }
     return 0;
 }
+
+/*
+*  vidmap
+*   DESCRIPTION: 
+*
+*   INPUTS: uint8_t ** start_screen
+*   OUTPUTS: 
+*   RETURN VALUE: 
+*/
 int32_t vidmap(uint8_t ** start_screen){
     return 0;
 }
+
+
+/*
+*  set_handler
+*   DESCRIPTION: 
+*
+*   INPUTS: uint32_t signum
+*           void * handler_address
+*   OUTPUTS: 
+*   RETURN VALUE: 
+*/
 int32_t set_handler(int32_t signum, void * handler_address){
     return 0;
 }
+
+
+/*
+*  sigreturn
+*   DESCRIPTION: 
+*
+*   INPUTS: none
+*   OUTPUTS: 
+*   RETURN VALUE: 
+*/
 int32_t sigreturn(void){
     return 0;
 }
